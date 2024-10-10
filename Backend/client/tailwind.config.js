5b04dabcc72177dc24017c1f1339516b04da047f
Juan <juanndiiego2@gmail.com>
2024-10-10 04:51:07 -0500

    Se crea frontend de diseño del login con funcionalidad basicas / flata creacion y conexion con el backend

--- Backend/client/tailwind.config.js
+++ Backend/client/tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    './src/**/*.{html,ts}', // Ajusta las rutas si es necesario
+  ],
   theme: {
     extend: {},
   },
