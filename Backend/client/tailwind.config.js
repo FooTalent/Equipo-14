99344864d8d143102dd56eccbbd501b85193001b
Juan <juanndiiego2@gmail.com>
2024-10-09 22:25:45 -0500

    Se crea rama Sidebar para desarrollo

--- Backend/client/tailwind.config.js
+++ Backend/client/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{html,ts}", // Archivos donde Tailwind busca las clases
+  ],
   theme: {
     extend: {},
   },
